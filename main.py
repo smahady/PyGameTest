182e026cee09b0e370c7723b7d2bac14ffd6fddd
Sean Mahady <52180359+smahady@users.noreply.github.com>
2021-07-17 05:14:53 +0000

    .

--- main.py
+++ main.py
@@ -133,11 +133,11 @@ class GroundEnemy(BaseEnemy):
 				movementY = 0
 
 				#find out if the main character is to the left of the enemy, if so move toward them - Kamille
-				if self.scene.main.posX < self.posX:
+				if self.game.main.posX < self.posX:
 					movementX = -1
 
 				# find out if the main character is to the right of the enemy, if so move toward them - Raphael
-				if self.scene.main.posX > self.posX:
+				if self.game.main.posX > self.posX:
 					movementX = 1
 				
 				# move at random speed 
