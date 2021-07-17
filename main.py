202661ab8c079bdf350d3f0d10b5c9cd6e7fd92e
Sean Mahady <52180359+smahady@users.noreply.github.com>
2021-07-17 05:13:43 +0000

    .

--- main.py
+++ main.py
@@ -84,6 +84,7 @@ class Spaceship(Spritesheet):
 		elif temp ==2:
 			newEnemy = FlyingEnemy(self.game, self.posX, self.posY)
 		self.enemies.append(newEnemy)
+		self.sprites.add(newEnemy)
 
 # Abstract base class - a base class we intend to inherit in another class
 class BaseEnemy(Spritesheet):
